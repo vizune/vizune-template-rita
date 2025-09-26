44099d76a7aaba1501bbc18e78b868363437c39c
Sasha Richardson <sasha_richardson@hotmail.co.uk>
2025-09-27 00:02:45 +0100

    More progress on Body UI / Component structure

--- rita/astro.config.mjs
+++ rita/astro.config.mjs
@@ -2,9 +2,15 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@tailwindcss/vite'
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwind()],
-  }
+    resolve: {
+      alias: {
+        '@components': '/src/components',
+        '@ui': '/src/components/ui',
+        '@sections': '/src/components/sections'
+      }
+    }
+  },
 })
---
+++ rita/src/components/index.ts
@@ -0,0 +1,13 @@
+export { default as Body } from './layout/Body.astro';
+export { default as Header } from './layout/Header.astro';
+export { default as Navigation } from './layout/Navigation.astro';
+export { default as NavigationLink } from './layout/NavigationLink.astro'
+export { default as Footer } from './layout/Footer.astro';
+
+export { default as Button } from './ui/Button.astro';
+export { default as Icon } from './ui/Icon.astro';
+export { default as IconSprite } from './ui/IconSprite.astro';
+export { default as Mandala } from './ui/Mandala.astro';
+
+export { default as Social } from './sections/Social.astro';
+export { default as Affiliates } from './sections/Affiliates.astro';
